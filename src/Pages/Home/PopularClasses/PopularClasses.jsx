5a23d52032d4290a2628cd1aad41bdebac1cb707
tirahman105 <tirahman105@gmail.com>
2023-06-07 02:04:06 +0600

    section title added

---
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+const PopularClasses = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"Discover Our Popular English Courses for Every Skill Level"}
+        heading={"Popular Classes"}
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularClasses;
