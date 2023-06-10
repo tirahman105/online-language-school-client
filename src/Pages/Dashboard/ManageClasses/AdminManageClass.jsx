53c2c0ceb249b7d26bb813f982a1883997d62b33
tirahman105 <tirahman105@gmail.com>
2023-06-11 01:30:53 +0600

    feedback message done

--- src/Pages/Dashboard/ManageClasses/AdminManageClass.jsx
+++ src/Pages/Dashboard/ManageClasses/AdminManageClass.jsx
@@ -1,14 +1,171 @@
+// import { useQuery } from "@tanstack/react-query";
+// import useAxiosSecure from "../../../hooks/useAxiosSecure";
+// import Swal from "sweetalert2";
+// import { TiTick, TiTimes } from "react-icons/ti";
+// import { useState } from "react";
+
+// const AdminManageClass = () => {
+//   const [axiosSecure] = useAxiosSecure();
+//   const { data: classes = [], refetch } = useQuery(["classes"], async () => {
+//     const res = await axiosSecure.get("/classes");
+//     return res.data;
+//   });
+
+//   const [modal, setModal] = useState(false);
+
+//   const handleApprove = (item) => {
+//     console.log("approve");
+//     fetch(`http://localhost:5000/classes/${item._id}`, {
+//       method: "PATCH",
+//       body: JSON.stringify({ status: "approved" }),
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//         if (data.modifiedCount) {
+//           refetch();
+//           Swal.fire({
+//             position: "top-end",
+//             icon: "success",
+//             title: `${item.name} is approved Now!`,
+//             showConfirmButton: false,
+//             timer: 1500,
+//           });
+//         }
+//       });
+//   };
+
+//   const handleDeny = (event, item) => {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     console.log("deny");
+
+//     fetch(`http://localhost:5000/classes/${item._id}`, {
+//       method: "PATCH",
+//       body: JSON.stringify({ status: "denied" }),
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//         if (data.modifiedCount) {
+//           refetch();
+//           Swal.fire({
+//             position: "top-end",
+//             icon: "success",
+//             title: `${item.name} is denied Now!`,
+//             showConfirmButton: false,
+//             timer: 1500,
+//           });
+//         }
+//       });
+//   };
+//   return (
+//     <div>
+//       <h1 className="text-center">Total Classes: {classes.length}</h1>
+//       <div className="overflow-x-auto">
+//         <table className="table table-zebra w-full">
+//           {/* head */}
+//           <thead>
+//             <tr>
+//               <th>#</th>
+//               <th>Class</th>
+//               <th>Instructor name</th>
+//               <th>Instructor email</th>
+//               <th>Status</th>
+//               <th>Action</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {classes.map((item, index) => (
+//               <tr key={item._id}>
+//                 <th>{index + 1}</th>
+//                 <td>{item.name}</td>
+//                 <td>{item.instructor}</td>
+//                 <td>{item.email}</td>
+
+//                 <td>
+//                   {item.status === "pending" ? (
+//                     <>
+//                       <button
+//                         onClick={() => handleApprove(item)}
+//                         className="btn btn-success text-white"
+//                       >
+//                         Approve
+//                       </button>
+//                       <button
+//                         onClick={(event) => handleDeny(event, item)}
+//                         className="btn btn-ghost bg-orange-600 text-white"
+//                       >
+//                         Deny
+//                       </button>
+//                     </>
+//                   ) : item.status === "approved" ? (
+//                     <button className="btn btn-success text-white">
+//                       Approved
+//                     </button>
+//                   ) : (
+//                     <button className="btn btn-warning text-white">
+//                       Denied
+//                     </button>
+//                   )}
+//                 </td>
+
+//                 <td>
+//                   {/* Open the modal using ID.showModal() method */}
+//                   <button
+//                     className="btn"
+//                     onClick={() => item._id.showModal()}
+//                   >
+//                     open modal
+//                   </button>
+//                   <dialog
+//                     id={item._id}
+//                     className="modal modal-bottom sm:modal-middle"
+//                   >
+//                     <form method="dialog" className="modal-box">
+//                       <h3 className="font-bold text-lg">Hello!</h3>
+//                       <p className="py-4">
+//                         Press ESC key or click the button below to close
+//                       </p>
+//                       <div className="modal-action">
+//                         {/* if there is a button in form, it will close the modal */}
+//                         <button className="btn">Close</button>
+//                       </div>
+//                     </form>
+//                   </dialog>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AdminManageClass;
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
-import { TiTick, TiTimes } from "react-icons/ti";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const AdminManageClass = () => {
+  const { register, handleSubmit, reset } = useForm();
   const [axiosSecure] = useAxiosSecure();
   const { data: classes = [], refetch } = useQuery(["classes"], async () => {
     const res = await axiosSecure.get("/classes");
     return res.data;
   });
+
+  const [modal, setModal] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
 
   const handleApprove = (item) => {
     console.log("approve");
@@ -16,7 +173,7 @@ const AdminManageClass = () => {
       method: "PATCH",
       body: JSON.stringify({ status: "approved" }),
       headers: {
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
       },
     })
       .then((res) => res.json())
@@ -35,16 +192,47 @@ const AdminManageClass = () => {
       });
   };
 
-  const handleDeny = (event, item) => {
+  // const handleDeny = (event, item) => {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   console.log("deny");
+
+  //   fetch(`http://localhost:5000/classes/${item._id}`, {
+  //     method: "PATCH",
+  //     body: JSON.stringify({ status: "denied" }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       if (data.modifiedCount) {
+  //         refetch();
+  //         Swal.fire({
+  //           position: "top-end",
+  //           icon: "success",
+  //           title: `${item.name} is denied Now!`,
+  //           showConfirmButton: false,
+  //           timer: 1500,
+  //         });
+  //       }
+  //     });
+  // };
+
+  const handleDenyFeedback = (event, item) => {
     event.preventDefault();
     event.stopPropagation();
     console.log("deny");
 
+    const formData = new FormData(event.target);
+    const feedback = formData.get("feedback");
+
     fetch(`http://localhost:5000/classes/${item._id}`, {
       method: "PATCH",
-      body: JSON.stringify({ status: "denied" }),
+      body: JSON.stringify({ status: "denied", feedback }),
       headers: {
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
       },
     })
       .then((res) => res.json())
@@ -62,12 +250,16 @@ const AdminManageClass = () => {
         }
       });
   };
+
+  const handleCloseModal = () => {
+    setSelectedItemId(null);
+  };
+
   return (
     <div>
       <h1 className="text-center">Total Classes: {classes.length}</h1>
       <div className="overflow-x-auto">
         <table className="table table-zebra w-full">
-          {/* head */}
           <thead>
             <tr>
               <th>#</th>
@@ -80,53 +272,71 @@ const AdminManageClass = () => {
           </thead>
           <tbody>
             {classes.map((item, index) => (
-              <tr key={item._id}>
+              <tr key={item._id} className={selectedItemId === item._id ? "modal-open" : ""}>
                 <th>{index + 1}</th>
                 <td>{item.name}</td>
                 <td>{item.instructor}</td>
                 <td>{item.email}</td>
 
-         
-
-
-<td>
-  {item.status === 'pending' ? (
-    <>
-      <button onClick={() => handleApprove(item)} className="btn btn-success text-white">Approve</button>
-      <button onClick={(event) => handleDeny(event, item)} className="btn btn-ghost bg-orange-600 text-white">Deny</button>
-    </>
-  ) : item.status === 'approved' ? (
-    <button className="btn btn-success text-white">Approved</button>
-  ) : (
-    <button className="btn btn-warning text-white">Denied</button>
-  )}
-</td>
-
-
                 <td>
-                  {/* Open the modal using ID.showModal() method */}
-                  <button
-                    className="btn"
-                    onClick={() => window.my_modal_5.showModal()}
-                  >
-                    open modal
+                  {item.status === "pending" ? (
+                    <>
+                      <button
+                        onClick={() => handleApprove(item)}
+                        className="btn btn-success text-white"
+                      >
+                        Approve
+                      </button>
+                      {/* <button
+                        onClick={(event) => handleDeny(event, item)}
+                        className="btn btn-ghost bg-orange-600 text-white"
+                      >
+                        Deny
+                      </button> */}
+                      <>
+                      <button className="btn btn-error" onClick={() => setSelectedItemId(item._id)}>
+                    Deny
                   </button>
-                  <dialog
-                    id="my_modal_5"
-                    className="modal modal-bottom sm:modal-middle"
-                  >
-                    <form method="dialog" className="modal-box">
-                      <h3 className="font-bold text-lg">Hello!</h3>
-                      <p className="py-4">
-                        Press ESC key or click the button below to close
-                      </p>
-                      <div className="modal-action">
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="btn">Close</button>
-                      </div>
-                    </form>
-                  </dialog>
+                  {selectedItemId === item._id && (
+                    <dialog id={item._id} className="modal modal-bottom sm:modal-middle" open>
+                      <form
+                        onSubmit={(event) => handleDenyFeedback(event, item)}
+                        method="dialog"
+                        className="modal-box"
+                      >
+                        <h3 className="font-bold text-lg">{item.name}</h3>
+                        <div className="form-control">
+                          <label className="label">
+                            <span className="label-text">Course Details</span>
+                          </label>
+                          <textarea
+                            {...register("feedback", { required: true })}
+                            className="textarea textarea-bordered h-24"
+                            placeholder="Write about your course"
+                          ></textarea>
+                        </div>
+                        <div className="modal-action">
+                          <button type="submit" className="btn">
+                            Deny Message
+                          </button>
+                          <button type="button" className="btn btn-error" onClick={handleCloseModal}>
+                            Close
+                          </button>
+                        </div>
+                      </form>
+                    </dialog>
+                  )}
+                      
+                      </>
+                    </>
+                  ) : item.status === "approved" ? (
+                    <button className="btn btn-success text-white">Approved</button>
+                  ) : (
+                    <button className="btn btn-warning text-white">Denied</button>
+                  )}
                 </td>
+
+               
               </tr>
             ))}
           </tbody>
@@ -138,8 +348,3 @@ const AdminManageClass = () => {
 
 export default AdminManageClass;
 
-{
-  /* <td>{ item.status === 'pending' ? <button onClick={() => handleApprove(item)} className="btn btn-ghost bg-orange-600  text-white">Approve</button>  :<button  className="btn btn-success  text-white">Approved</button> 
-                        
-                    }</td> */
-}
