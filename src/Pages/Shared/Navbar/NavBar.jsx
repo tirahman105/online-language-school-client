d4d5ca72066963618f925a25eea3686e60d3b544
tirahman105 <tirahman105@gmail.com>
2023-06-13 02:09:03 +0600

    navbar correction

--- src/Pages/Shared/Navbar/NavBar.jsx
+++ src/Pages/Shared/Navbar/NavBar.jsx
@@ -53,9 +53,9 @@ useEffect(() => {
     return (
         <>
        
-<div className="navbar dark:bg-slate-900 max-w-screen-xl mx-auto bg-blue-900 text-white rounded-lg">
+<div className="navbar z-10 dark:bg-slate-900 max-w-screen-xl mx-auto bg-blue-900 text-white rounded-lg">
   <div className="navbar-start">
-    <div className="dropdown">
+    <div className="dropdown z-20">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
